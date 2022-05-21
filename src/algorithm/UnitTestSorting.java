f0e3fdad5316d803279328cc9935532821494800
king.fm.gm@gmail.com <king.fm.gm>
2022-05-20 21:58:36 -0400

    fourth commit

--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -1,5 +1,5 @@
 package algorithm;
-
+import java.util.Arrays;
 import org.junit.Assert;
 
 public class UnitTestSorting {
@@ -8,67 +8,34 @@ public class UnitTestSorting {
       This class is about Unit testing for Sorting Algorithm.
      */
     public static void main(String[] args) {
-        int [] unSortedArray = {6,9,2,5,1,0,4};
-        int [] sortedArray =   {0,1,2,4,5,6,9};
-        //Create Sort object
-        Sort sort = new Sort();
-        //apply unsorted array to selectionSort.
-        sort.selectionSort(unSortedArray);
-        //verify if the unsorted array is sorted by the selection sort algorithm.
-        try {
-            Assert.assertEquals(sortedArray, unSortedArray);
-        }catch(Exception ex){
-            ex.getMessage();
-        }
-
-        //Now implement Unit test for rest of the soring algorithm...................below
-
-    }
-    @Test
-    public void selectionSortTesting(){
-        int [] unSortedArray = {6,9,2,5,1,0,4};
-        int [] sortedArray =   {0,1,2,4,5,6,9};
-        //Create Sort object
-        Sort sort = new Sort();
-        int[] expectedArray = sortedArray;
-        int[] actualArray = sort.selectionSort(unSortedArray);
-        try {
-            Assert.assertEquals("Array is not Sorted", expectedArray, Arrays.toString(actualArray));
-        }catch(Exception ex){
-            ex.getMessage();
-        }
-    }
-
-    @Test
-    public void insertionSortTesting(){
-        int [] unSortedArray = {6,9,2,5,1,0,4};
-        int [] sortedArray =   {0,1,2,4,5,6,9};
-        //Create Sort object
-        Sort sort = new Sort();
-        int[] expectedArray = sortedArray;
-        int[] actualArray = sort.insertionSort(unSortedArray);
-        try {
-            Assert.assertEquals("Array is not Sorted", expectedArray, String.valueOf(actualArray));
-        }catch(Exception ex){
-            ex.getMessage();
-        }
-    }
-
-    @Test
-    public void bubbleSortTesting() {
         int[] unSortedArray = {6, 9, 2, 5, 1, 0, 4};
         int[] sortedArray = {0, 1, 2, 4, 5, 6, 9};
-        //Create Sort object
+
+        // Create instance of Sort class
         Sort sort = new Sort();
-        int[] expectedArray = sortedArray;
-        int[] actualArray = sort.bubbleSort(unSortedArray);
+
+        // Pass the unsorted array to selectionSort() method from Sort class
+        sort.selectionSort(unSortedArray);
+
+
+
+        // Verify if the unsorted array is sorted by the selectionSort() method
         try {
-            Assert.assertEquals("Array is not Sorted", expectedArray, Arrays.toString(actualArray));
+            assertEquals("ARRAY IS NOT SORTED... YET!\n***YOU GOT THIS!***", sortedArray, unSortedArray);
         } catch (Exception ex) {
             ex.getMessage();
         }
 
+        // Implement Unit test for rest of the sorting algorithms below
 
+        sort.bubbleSort(unSortedArray);
+        System.out.println(Arrays.toString(unSortedArray));
+        sort.mergeSort(unSortedArray,6);
+        System.out.println(Arrays.toString(unSortedArray));
+        sort.quickSort(sortedArray,0,6);
+        System.out.println(Arrays.toString(unSortedArray));
+        sort.shellSort(unSortedArray);
+        System.out.println(Arrays.toString(unSortedArray));
 
     }
 }
